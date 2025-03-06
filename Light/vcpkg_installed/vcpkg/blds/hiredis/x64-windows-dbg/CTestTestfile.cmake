33841e2d20aad0b4e9142eca558f2b38cfd9cc42
FIRM3p1 <123132@example.com>
2025-03-07 01:02:37 +0300

    Nikolay redis

---
+++ Light/vcpkg_installed/vcpkg/blds/hiredis/x64-windows-dbg/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Admin/source/repos/Light/Light/vcpkg_installed/vcpkg/blds/hiredis/src/v1.0.0-40894b8fe5.clean
+# Build directory: C:/Users/Admin/source/repos/Light/Light/vcpkg_installed/vcpkg/blds/hiredis/x64-windows-dbg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hiredis-test "C:/Users/Admin/source/repos/Light/Light/vcpkg_installed/vcpkg/blds/hiredis/src/v1.0.0-40894b8fe5.clean/test.sh")
+set_tests_properties(hiredis-test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Admin/source/repos/Light/Light/vcpkg_installed/vcpkg/blds/hiredis/src/v1.0.0-40894b8fe5.clean/CMakeLists.txt;158;ADD_TEST;C:/Users/Admin/source/repos/Light/Light/vcpkg_installed/vcpkg/blds/hiredis/src/v1.0.0-40894b8fe5.clean/CMakeLists.txt;0;")
